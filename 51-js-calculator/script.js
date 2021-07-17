89b93f4d6880aee46b9b9dd0cd40457a94b1d739
Arash <avsjnk@gmail.com>
2021-07-17 19:45:57 +0530

    updating

--- 51-js-calculator/script.js
+++ 51-js-calculator/script.js
@@ -1,1 +1,29 @@
-console.log();
+const $lis = document.querySelectorAll("ul li")
+$lis.forEach((node) => {
+    node.addEventListener('mousedown', function(event){
+        event.preventDefault()
+        const value = node.innerText.trim()
+        const $result = document.querySelector(".result")
+        const resultText = $result.innerText.trim()
+
+        if(resultText == '0') {
+            $result.innerText = ''
+        }
+
+        if(value == '=') {
+            let solution = eval(resultText)
+            $result.innerText = solution
+            return
+        }
+
+
+        if(value.toLowercase == 'c') {
+            $result.innerText = "I AM CLEARED"
+            return true        
+        }
+
+        
+
+        $result.append(value)
+    }
+})
